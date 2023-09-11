10f115da340f141fb395320ca21bdba7baf1b7a3
Afwana <afwanakp@gmail.com>
2023-09-11 09:28:48 +0530

    employee portall server

---
+++ index.js
@@ -0,0 +1,26 @@
+// steps to generate json server
+
+// 1. import json-sever
+const jsonServer = require ('json-server')
+
+// 2. create our own server to run json file
+const server = jsonServer.create()
+
+// 3. generate middleware to use in jsonserver
+const middleware = jsonServer.defaults()
+
+// 4. set up path for db.json 
+const router = jsonServer.router("db.json")
+
+// 5. set up port for server
+const port =  3000 || process.env.PORT
+    // process is a node to chose available port 
+
+// 6. use middleware, router, to server
+server.use(middleware)
+server.use(router)
+
+// 7. server listen / run to request
+server.listen(port,()=>{
+    console.log("Employee portal server started at port No : " +port);
+})
